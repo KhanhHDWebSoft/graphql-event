9f06cb373cfab510fa4e9d3c4d1df43fae9a5335
khanh nguyen <khanh.nguyen@hdwebsoft.co>
2022-03-17 21:14:03 +0700

    new code

--- src/entity/Event.ts
+++ src/entity/Event.ts
@@ -1,16 +1,22 @@
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@ObjectType()
 @Entity()
 export class Event extends BaseEntity {
-  @PrimaryColumn()
+  @Field(() => Int, { nullable: true })
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Field({ nullable: true })
   @Column()
   event_name: string;
 
+  @Field({ nullable: true })
   @Column("int", { default: 10, nullable: true })
   total_voucher: number;
 
+  @Field({ nullable: true })
   @Column("int", { default: 0, nullable: true })
   count_voucher: number;
 }
--- src/resolvers/EventResolver.ts
+++ src/resolvers/EventResolver.ts
@@ -1,12 +1,91 @@
 import { Event } from "../entity/Event";
-import { Arg, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Mutation,
+  Resolver,
+  Query,
+  InputType,
+  Field,
+  Int,
+} from "type-graphql";
+
+@InputType()
+class EventInput {
+  @Field()
+  eventName: string;
+
+  @Field({ nullable: true })
+  totalVoucher?: number;
+
+  @Field({ nullable: true })
+  voucherCount?: number;
+}
+
+@InputType()
+class UpdateEventInput {
+  @Field()
+  eventName: string;
+
+  @Field({ nullable: true })
+  totalVoucher?: number;
+}
 
 @Resolver()
 export class EventResolver {
+  @Mutation(() => Event)
+  async createEvent(@Arg("eventParams") eventParams: EventInput) {
+    const { eventName, totalVoucher, voucherCount } = eventParams;
+    const data = await Event.create({
+      event_name: eventName,
+      count_voucher: voucherCount,
+      total_voucher: totalVoucher,
+    }).save();
+
+    console.log(data);
+    return data;
+  }
+
   @Mutation(() => Boolean)
-  CreateEevent(@Arg("eventName", () => String) eventName: string) {
-    console.log("EVENT NAME", eventName);
-    Event.insert({ event_name: eventName });
+  async updateEvent(
+    @Arg("eventId", () => Int) id: number,
+    @Arg("eventUpdateParams") eventUpdateParams: UpdateEventInput
+  ) {
+    console.log(eventUpdateParams);
+    const { totalVoucher, eventName } = eventUpdateParams;
+    let updatedEvent;
+    const event = await Event.findOne({
+      where: {
+        id,
+      },
+    });
+    if (event) {
+      if (!totalVoucher) {
+        // updatedEvent = await Event.save({
+        //   id,
+        //   event_name: eventName,
+        // });
+      }
+      if (totalVoucher && totalVoucher < event.count_voucher) {
+        throw new Error(
+          "Current vouchers is higher than your new total voucher"
+        );
+      }
+
+      if (totalVoucher && totalVoucher >= event.count_voucher) {
+        updatedEvent = await Event.update(
+          { id },
+          { event_name: eventName, total_voucher: totalVoucher }
+        );
+      }
+    }
+
+    console.log(updatedEvent, "....");
+
     return true;
+  }
+
+  @Query(() => [Event])
+  events() {
+    return Event.find();
   }
 }
